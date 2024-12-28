18f958b86824c5f799bd9cb71036a8d368c7ffd2
Dmitriy Lyuosev <xhaustful@gmail.com>
2024-12-29 02:19:18 +0300

    feat: Add choosing between postgres, mysql and sqlite

--- draft_table.py
+++ draft_table.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import datetime
 import psycopg2
-from sqlalchemy import create_engine
+import sqlite3
+from sqlalchemy import create_engine, inspect
 import pandas as pd
 import numpy as np
 import base64
@@ -58,8 +59,8 @@ def color_cells(val):
             return 'color: green'
     return ''
 
-styled_df = st.session_state.df.style.applymap(color_cells)
-st.dataframe(styled_df, use_container_width=True)
+# styled_df = st.session_state.df.style.applymap(color_cells)
+st.dataframe(st.session_state.df, use_container_width=True)
 
 
 # Decode base64 encoded connection and query if provided
@@ -72,30 +73,67 @@ else:
 if 'query' not in st.session_state: st.session_state.query = data.get("query", "SELECT * FROM your_table LIMIT 10;")
 query = st.text_area("SQL Query", key="query")
 
-if 'db_host' not in st.session_state: st.session_state.db_host = data.get("db_host", "localhost")
-if 'db_port' not in st.session_state: st.session_state.db_port = int(data.get("db_port", 5432))
-if 'db_user' not in st.session_state: st.session_state.db_user = data.get("db_user", "your_username")
-if 'db_password' not in st.session_state: st.session_state.db_password = data.get("db_password", "your_password")
-if 'db_name' not in st.session_state: st.session_state.db_name = data.get("db_name", "your_database")
+if 'db_mode' not in st.session_state: st.session_state.db_mode = data.get("db_mode", "postgres")
+db_mode = st.selectbox("Database Mode", options=["postgres", "sqlite", "mysql"], index=0, key="db_mode")
+
+if db_mode == "postgres":
+    if 'db_host' not in st.session_state: st.session_state.db_host = data.get("db_host", "localhost")
+    if 'db_port' not in st.session_state: st.session_state.db_port = int(data.get("db_port", 5432))
+    if 'db_user' not in st.session_state: st.session_state.db_user = data.get("db_user", "your_username")
+    if 'db_password' not in st.session_state: st.session_state.db_password = data.get("db_password", "your_password")
+    if 'db_name' not in st.session_state: st.session_state.db_name = data.get("db_name", "your_database")
+
+elif db_mode == "sqlite":
+    if 'db_path' not in st.session_state: st.session_state.db_path = data.get("db_path", "your_database.db")
+
+elif db_mode == "mysql":
+    if 'db_host' not in st.session_state: st.session_state.db_host = data.get("db_host", "localhost")
+    if 'db_port' not in st.session_state: st.session_state.db_port = int(data.get("db_port", 3306))
+    if 'db_user' not in st.session_state: st.session_state.db_user = data.get("db_user", "your_username")
+    if 'db_password' not in st.session_state: st.session_state.db_password = data.get("db_password", "your_password")
+    if 'db_name' not in st.session_state: st.session_state.db_name = data.get("db_name", "your_database")
 
 
 def update_df():
     data = {
-        "db_host": st.session_state.db_host,
-        "db_port": int(st.session_state.db_port),
-        "db_user": st.session_state.db_user,
-        "db_password": st.session_state.db_password,
-        "db_name": st.session_state.db_name,
-        "query": query,
+        "db_mode": db_mode,
     }
+    if db_mode == "postgres":
+        data["db_host"] = st.session_state.db_host
+        data["db_port"] = int(st.session_state.db_port)
+        data["db_user"] = st.session_state.db_user
+        data["db_password"] = st.session_state.db_password
+        data["db_name"] = st.session_state.db_name
+    elif db_mode == "sqlite":
+        data["db_path"] = st.session_state.db_path
+    elif db_mode == "mysql":
+        data["db_host"] = st.session_state.db_host
+        data["db_port"] = int(st.session_state.db_port)
+        data["db_user"] = st.session_state.db_user
+        data["db_password"] = st.session_state.db_password
+        data["db_name"] = st.session_state.db_name
+    data["query"] = query
     encoded_data_str = base64.b64encode(str(data).encode("utf-8")).decode("utf-8")
 
     st.write("query link:")
     pl=f"{PROTO}://{HOST}:{PORT}/?data={encoded_data_str}"
     st.page_link(pl, label=pl)
 
-    connection_string = f"postgresql://{data['db_user']}:{data['db_password']}@{data['db_host']}:{data['db_port']}/{data['db_name']}"
-    engine = create_engine(connection_string)
+    if db_mode == "postgres":
+        connection_string = f"postgresql://{st.session_state.db_user}:{st.session_state.db_password}@{st.session_state.db_host}:{st.session_state.db_port}/{st.session_state.db_name}"
+        engine = create_engine(connection_string)
+    elif db_mode == "sqlite":
+        engine = create_engine(f"sqlite:///{st.session_state.db_path}")
+    elif db_mode == "mysql":
+        connection_string = f"mysql+pymysql://{st.session_state.db_user}:{st.session_state.db_password}@{st.session_state.db_host}:{st.session_state.db_port}/{st.session_state.db_name}"
+        engine = create_engine(connection_string)
+    
+    inspector = inspect(engine)
+    st.write(inspector.get_table_names())
+    if db_mode == "postgres" or db_mode == "mysql":
+        # st.write(inspector.get_columns("Order"))
+        pass
+    
     df = pd.read_sql(query, engine)
 
     # Convert timestamp columns from nanoseconds to datetime
@@ -114,30 +152,27 @@ def update_df():
 if st.button("Run Query"):
     try:
         update_df()
-        # db_host = st.session_state.get("db_host", "localhost")
-        # db_port = st.session_state.get("db_port", 5432)
-        # db_user = st.session_state.get("db_user", "your_username")
-        # db_password = st.session_state.get("db_password", "your_password")
-        # db_name = st.session_state.get("db_name", "your_database")
-        # query = st.session_state.get("query", "SELECT * FROM your_table LIMIT 10;")
-        # Encode current inputs into a single base64 string
-
-        # Store input values in session state
-        # st.session_state.db_host = db_host
-        # st.session_state.db_port = db_port
-        # st.session_state.db_user = db_user
-        # st.session_state.db_password = db_password
-        # st.session_state.db_name = db_name
-        # st.session_state.query = query
     except Exception as e:
         st.error(f"Error: {e}")
 
 
-st.text_input("Database Host", key="db_host")
-st.number_input("Database Port", key="db_port", format='%u')
-st.text_input("Database User", key="db_user")
-st.text_input("Database Password", type="password", key="db_password")
-st.text_input("Database Name", key="db_name")
+if db_mode == "postgres":
+    st.text_input("Database Host", key="db_host")
+    st.number_input("Database Port", key="db_port", format='%u', value=5432)
+    st.text_input("Database User", key="db_user")
+    st.text_input("Database Password", type="password", key="db_password")
+    st.text_input("Database Name", key="db_name")
+
+elif db_mode == "sqlite":
+    st.text_input("Database Path", key="db_path")
+
+elif db_mode == "mysql":
+    st.text_input("Database Host", key="db_host")
+    st.number_input("Database Port", key="db_port", format='%u', value=3306)
+    st.text_input("Database User", key="db_user")
+    st.text_input("Database Password", type="password", key="db_password")
+    st.text_input("Database Name", key="db_name")
+
 
 # Add input field for timestamp to datetime conversion
 st.write("Input timestamp:")
